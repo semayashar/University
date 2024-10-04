2347eae12ed5624b89d96fce646901493c299326
Sema Yasharova <semayasharova@gmail.com>
2024-10-04 20:34:19 +0300

    Added new parking lot CSS

--- Internet Technologies/Exercises/restfull_webservice/src/main/java/config/RestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/webapi")
-public class RestConfiguration extends Application{
-
-}
--- Internet Technologies/Hotel Reservations P1/Hotel_Reservations/src/main/java/entities/IdGenerator.java
+++
@@ -1,9 +0,0 @@
-package entities;
-
-public class IdGenerator {
-    private static int idCounter = 0;
-
-    public static int generateId() {
-        return ++idCounter;
-    }
-}
